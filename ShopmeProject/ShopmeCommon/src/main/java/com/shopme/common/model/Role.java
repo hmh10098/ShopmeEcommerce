948207f5500b4afd2559550e5864e3a2bd8920ce
hmh10098 <65597836+hmh10098@users.noreply.github.com>
2022-04-28 11:40:44 +0700

    add User Entity

--- ShopmeProject/ShopmeCommon/src/main/java/com/shopme/common/model/Role.java
+++ ShopmeProject/ShopmeCommon/src/main/java/com/shopme/common/model/Role.java
@@ -1,5 +1,7 @@
 package com.shopme.common.model;
 
+import java.util.Objects;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -57,4 +59,32 @@ public class Role {
 	public Role() {
 	}
 
+	public Role(Integer id) {
+		this.id = id;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Role other = (Role) obj;
+		return Objects.equals(id, other.id);
+	}
+
+	@Override
+	public String toString() {
+		return "Role [name=" + name + "]";
+	}
+	
+	
+	
 }
---
+++ ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/test/java/com/shopme/admin/user/UserRepositoryTests.java
@@ -0,0 +1,63 @@
+package com.shopme.admin.user;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.annotation.Rollback;
+
+import com.shopme.admin.repository.UserRepository;
+import com.shopme.common.model.Role;
+import com.shopme.common.model.User;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace=Replace.NONE)
+@Rollback(false)
+public class UserRepositoryTests {
+	
+	@Autowired
+	private UserRepository repo;
+	
+	@Autowired
+	private TestEntityManager testEntityManager;
+	
+	@Test
+	public void testCreateNewUserWithOneRole() {
+		Role roleAdmin = testEntityManager.find(Role.class, 1);
+		User userHieuhm = new User("hieu@gmail.com", "hieu123", "Hieu", "Ha Minh");
+		userHieuhm.addRole(roleAdmin);
+		
+		User savedUser = repo.save(userHieuhm);
+		
+		assertThat(savedUser.getId()).isGreaterThan(0);
+		
+	}
+	
+	@Test
+	public void testCreateNewUserWithTwoRole() {
+		User userDuong = new User("duong@gmail.com", "duong12", "Duong", "Nguyen Sy");
+		
+		Role roleEditor = new Role(3);
+		Role roleAssistiant = new Role(5);
+		
+		userDuong.addRole(roleAssistiant);
+		userDuong.addRole(roleEditor);
+		
+		User savedUser = repo.save(userDuong);
+		
+		assertThat(savedUser.getId()).isGreaterThan(0);
+		
+	}
+	
+	@Test
+	public void testListAllUsers() {
+		Iterable<User> listUsers = repo.findAll();
+		listUsers.forEach(user->System.out.print(user));
+		//assertThat(listUsers.)
+		
+	}
+}
